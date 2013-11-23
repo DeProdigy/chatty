8713036a26428c08aa6ee6df319d01e7bf4626bf
DeProdigy <alex.hint.ah@gmail.com>
2013-11-23 18:40:43 -0500

    created messages table and fixed models

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131123231312) do
+ActiveRecord::Schema.define(:version => 20131123233819) do
 
   create_table "chatrooms", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "messages", :force => true do |t|
+    t.text     "content"
+    t.integer  "chatroom_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
